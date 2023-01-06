d17c7a622cdd37364b89c59a0e43fc9bfca71551
IlyaRemorsA <blackbaggetjack@gmail.com>
2023-01-06 20:26:49 +0300

    dd

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
 		{
-			path: '/car',
+			path: '/',
 			name: 'main',
 			component: () => import('../pages/Car.vue'),
 		}
