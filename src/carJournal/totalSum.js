39964c34a588dc7eaaadb16e3c2be893665fde0d
RemorsA <87796386+RemorsA@users.noreply.github.com>
2023-01-07 18:05:07 +0300

    Update totalSum.js

--- src/carJournal/totalSum.js
+++ src/carJournal/totalSum.js
@@ -2,7 +2,7 @@ export function totalSum(sum) {
     let total = 0
 
     sum.forEach((el) => {
-        total += Number(el.sum.split(' ').join(''))
+        total += Number(el.sum.split('.').join(''))
     })
 
     return total
